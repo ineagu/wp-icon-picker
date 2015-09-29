afda5a61df00991e96b3efeb6818dc6858549f42
Dzikri Aziz <kvcrvt@gmail.com>
2015-09-29 15:28:35 +0700

    Tests: Add test_loader()

--- tests/test-load-plugin.php
+++ tests/test-load-plugin.php
@@ -20,4 +20,20 @@ class LoadPluginTest extends WP_UnitTestCase {
 		$this->assertNotEmpty( $instance->registry->types );
 		$this->assertContainsOnlyInstancesOf( 'Icon_Picker_Type', $instance->registry->types );
 	}
+
+
+	public function test_loader() {
+		$instance = Icon_Picker::instance();
+
+		$this->assertInstanceOf( 'Icon_Picker_Loader', $instance->loader );
+
+		$this->assertNotEmpty( $instance->loader->script_ids );
+		foreach ( $instance->loader->script_ids as $script_id ) {
+			$this->assertTrue( wp_script_is( $script_id, 'registered' ) );
+		}
+
+		foreach ( $instance->loader->style_ids as $style_id ) {
+			$this->assertTrue( wp_style_is( $style_id, 'registered' ) );
+		}
+	}
 }
