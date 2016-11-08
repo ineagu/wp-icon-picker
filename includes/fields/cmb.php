7a8bc87b2088640bf6db0d19a4c57d3a09cf6ccd
Dzikri Aziz <kvcrvt@gmail.com>
2016-11-08 21:48:58 +0800

    Improve support for CMB
    Make the field work in a repeatable field.

--- includes/fields/cmb.php
+++ includes/fields/cmb.php
@@ -4,7 +4,7 @@
  * 'icon' field type for Custom Meta Boxes
  *
  * @link    https://github.com/humanmade/Custom-Meta-Boxes/wiki/Adding-your-own-field-types CMB Wiki
- * @version 0.1.0
+ * @version 0.1.1
  * @since   Icon_Picker 0.2.0
  */
 class Icon_Picker_Field_Cmb extends CMB_Field {
@@ -19,6 +19,18 @@ class Icon_Picker_Field_Cmb extends CMB_Field {
 		Icon_Picker::instance()->load();
 	}
 
+	/**
+	 * Parse save values
+	 *
+	 * When used as a sub-field of a `group` field, wrap the values with array.
+	 *
+	 * @since 0.1.1
+	 */
+	public function parse_save_values() {
+		if ( ! empty( $this->parent ) ) {
+			$this->values = array( $this->values );
+		}
+	}
 
 	/**
 	 * Display the field
