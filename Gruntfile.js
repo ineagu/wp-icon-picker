cec30be7ac94a718e7abd1e65da55394e7791ef6
Dzikri Aziz <kvcrvt@gmail.com>
2017-01-29 15:55:43 +0800

    Add cssmin & uglify grunt packages & tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,29 @@
 module.exports = function( grunt ) {
 	// Project configuration.
 	grunt.initConfig({
+		uglify: {
+			all: {
+				files: {
+					'js/icon-picker.min.js': ['js/icon-picker.js']
+				}
+			}
+		},
+		cssmin: {
+			main: {
+				expand: true,
+				cwd: 'css/',
+				src: ['icon-picker.css'],
+				dest: 'css/',
+				ext: '.min.css'
+			},
+			types: {
+				expand: true,
+				cwd: 'css/types/',
+				src: [ '*.css', '!*.min.css' ],
+				dest: 'css/types/',
+				ext: '.min.css'
+			}
+		},
 		makepot: {
 			target: {
 				options: {
@@ -13,9 +36,11 @@ module.exports = function( grunt ) {
 	});
 
 	// Load tasks.
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 
 	// Register tasks.
-	grunt.registerTask( 'i18n', ['makepot']);
+	grunt.registerTask( 'i18n', [ 'cssmin', 'uglify', 'makepot' ]);
 	grunt.registerTask( 'default', ['i18n']);
 };
